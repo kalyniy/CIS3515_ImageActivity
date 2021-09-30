f274c3bf7031e9467f6c4fbf82402377a7928790
playerplayer <bohdan.k2000@gmail.com>
2021-09-30 07:37:41 -0400

    Added pictures

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -1,11 +1,28 @@
 package edu.temple.imageactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class ImageActivity : AppCompatActivity() {
+
+class ImageActivity : AppCompatActivity()
+{
+    var s1: Array<String?>? = null
+    var s2: Array<String?>? = null
+    val images: IntArray = intArrayOf(  R.drawable.australian,
+                                        R.drawable.blackretriever,
+                                        R.drawable.chow,
+                                        R.drawable.corgi,
+                                        R.drawable.eskimo,
+                                        R.drawable.goldenretriever,
+                                        R.drawable.husky,
+                                        R.drawable.saluki,
+                                        R.drawable.samoyed,
+                                        R.drawable.shiba)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        s1 = resources.getStringArray(R.array.breeds);
+        s2 = resources.getStringArray(R.array.description);
     }
 }
